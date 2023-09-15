9e3e29249ab415f2ac8deb83b913b990e156e3ee
DESKTOP-HSE0BFQ\bch07 <bch0717@naver.com>
2023-09-15 21:38:57 +0900

    spring commit
    commit

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,18 +1,17 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-
+//@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    //@Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
